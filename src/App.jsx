a03a0163395e783213140506ca281c316bcdfb95
Sathis <rsathisk15@gmail.com>
2025-07-13 12:12:29 +0200

    Basic Routing added for all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/auth/SignIn';
 import SignUp from './pages/auth/SignUp';
+import Dashboard from './pages/Dashboard';
+import AccountPage from './pages/AccountPage';
+import Settings from './pages/Settings';
+import Transactions from './pages/Transactions';
+
+const RootLayout = () => {
+  const user = null;
+  return !user ? <Navigate to="/signIn" /> : <Outlet />;
+};
+
 function App() {
   return (
     <main>
       <Routes>
+        <Route element={<RootLayout />}>
+          <Route element={<Navigate to={'/overview'} />} />
+          <Route path="/overview" element={<Dashboard />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Route>
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
       </Routes>
