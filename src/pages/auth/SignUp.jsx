35e6c826adce261ee2de03965e60c06e5823d4f9
Sathis <rsathisk15@gmail.com>
2025-07-21 18:38:09 +0200

    Bug fix

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -60,7 +60,7 @@ const SignUp = () => {
 
   useEffect(() => {
     user && navigate('/');
-  }, [user]);
+  }, [user, navigate]);
 
   return (
     <div className="flex items-center justify-center w-full min-h-screen py-10">
