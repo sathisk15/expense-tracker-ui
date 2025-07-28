3ddfd3bf2c5dae73b5ccc82e640b6014b3f494b1
Sathis <rsathisk15@gmail.com>
2025-07-28 11:29:20 +0200

    Charts Components created for Dashboard

--- src/components/ui/Chart.jsx
+++ src/components/ui/Chart.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import Title from './Title';
+
+const data = [
+  { label: 'January', income: 5000, expense: 3000 },
+  { label: 'Febrary', income: 5000, expense: 3000 },
+  { label: 'March', income: 5000, expense: 3000 },
+  { label: 'April', income: 5000, expense: 3000 },
+  { label: 'May', income: 5000, expense: 3000 },
+  { label: 'June', income: 5000, expense: 3000 },
+  { label: 'July', income: 5000, expense: 3000 },
+  { label: 'August', income: 5000, expense: 3000 },
+  { label: 'September', income: 5000, expense: 3000 },
+  { label: 'October', income: 5000, expense: 3000 },
+  { label: 'November', income: 5000, expense: 3000 },
+  { label: 'December', income: 5000, expense: 3000 },
+];
 
 const Chart = () => {
-  return <div>Chart</div>;
+  return (
+    <div className="w-full md:w-2/3">
+      <Title title={'Transaction Activity'} />
+      <ResponsiveContainer width={'100%'} height={500} className={'mt-5'}>
+        <LineChart width={500} height={300} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <YAxis />
+          <XAxis />
+          <Legend />
+          <Line type="monotone" dataKey="income" stroke="#8884d8" />
+          <Line type="monotone" dataKey="expense" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Chart;
--- src/components/ui/DouhgnutChart.jsx
+++ src/components/ui/DouhgnutChart.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts';
+import Title from './Title';
+
+const data = [
+  { name: 'Income', value: 15565 },
+  { name: 'Expense', value: 5885 },
+];
+
+const COLORS = ['#0088FE', '#FFBB28', '#FF8042', '#00C49F'];
 
 const DoughnutChart = () => {
-  return <div>DoughnutChart</div>;
+  return (
+    <div className="w-full md:w-1/3 flex flex-col items-center bg-gray-50 dark:bg-transparent">
+      <Title title="Summary" />
+      <ResponsiveContainer width={'100%'} height={500}>
+        <PieChart width={500} height={400}>
+          <Tooltip />
+          <Legend />
+          <Pie
+            data={data}
+            innerRadius={110}
+            outerRadius={180}
+            fill="#8884D8"
+            paddingAngle={5}
+            dataKey={'value'}
+          >
+            {data.map((entry, index) => (
+              <Cell key={'cell' + index} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default DoughnutChart;
