cc93c0f847bc203589752e9aceaef27db99824b0
Sathis <rsathisk15@gmail.com>
2025-08-05 19:57:39 +0530

    Accounts and Transactions Componenets are added

--- src/components/ui/Accounts.jsx
+++ src/components/ui/Accounts.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FaBtc } from 'react-icons/fa';
+import { RiVisaLine } from 'react-icons/ri';
+import Title from './Title';
+
+const data = [
+  {
+    name: 'Crypto',
+    account: 'test@gmail.com',
+    amount: '85,323.97',
+    icon: (
+      <div className="w-12 h-12 bg-amber-600 text-white">
+        <FaBtc size={26} />
+      </div>
+    ),
+  },
+  {
+    name: 'Visa Debit Card',
+    account: '2342********9834',
+    amount: '65,323.97',
+    icon: (
+      <div className="w-12 h-12 bg-blue-600 text-white">
+        <RiVisaLine size={26} />
+      </div>
+    ),
+  },
+];
 
 const Accounts = () => {
-  return <div>Accounts</div>;
+  return (
+    <div className="mt-20 md:mt-0 py-5 md:py-20 md:w-1/3">
+      <Title title={'Accounts'} />
+      <span className="text-sm text-gray-600 dark:text-gray-500">
+        View all your accounts
+      </span>
+      <div className="w-full">
+        {data.map((item, idx) => (
+          <div key={idx} className="flex items-center justify-between mt-6">
+            <div className="flex items-center gap-4">{item.icon}</div>
+            <div>
+              <p className="text-black dark:text-gray-400 text-lg">
+                {item.name}
+              </p>
+              <span className="text-gray-600">{item.account}</span>
+            </div>
+            <div>
+              <p className="text-xl text-black dark:text-gray-400 font-medium">
+                ${item.amount}.00
+              </p>
+              <span className="text-xl text-gray-600 dark:text-violet-700">
+                Account Balance
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Accounts;
--- src/components/ui/Transactions.jsx
+++ src/components/ui/Transactions.jsx
@@ -1,7 +1,87 @@
-import React from 'react';
+import Title from './Title';
+import { IoCheckmarkDoneCircle } from 'react-icons/io5';
+import { RiProgress1Line } from 'react-icons/ri';
+import { TiWarning } from 'react-icons/ti';
+
+const data = [
+  {
+    date: '2024-03-03',
+    name: 'Online Shoping',
+    status: 'Completed',
+    source: 'Credit Card',
+    amount: 180,
+  },
+  {
+    date: '2024-03-03',
+    name: 'Online Shoping',
+    status: 'Completed',
+    source: 'Credit Card',
+    amount: 180,
+  },
+  {
+    date: '2024-03-03',
+    name: 'Online Shoping',
+    status: 'Completed',
+    source: 'Credit Card',
+    amount: 180,
+  },
+];
 
 const Transactions = () => {
-  return <div>Transactions</div>;
+  return (
+    <div className="py-20 w-full md:w-2/3">
+      <Title title="Latest Transactions" />
+      <div className="overflow-x-auto mt-5">
+        <table className="w-full">
+          <thead className="w-full border-b border-gray-300 dark:border-gray-700">
+            <tr className="w-full text-black dark:text-gray-400 text-left">
+              <th className="py-2">Date</th>
+              <th className="py-2">Name</th>
+              <th className="py-2 ">Status</th>
+              <th className="py-2">Source</th>
+              <th className="py-2">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr
+                key={index}
+                className="border-b border-gray-200 dark:border-gray-700 text-gray-600 dark:text-gray-500 hover:bg-gray-300/10"
+              >
+                <td className="py-2 px-2">{row.date}</td>
+                <td className="py-2 px-2">
+                  <div>
+                    <p className="font-medium text-lg text-black dark:text-gray-400">
+                      {row.name}
+                    </p>
+                    <span className="text-sm text-gray-600">{row.contact}</span>
+                  </div>
+                </td>
+                <td className="py-2 px-2 flex items-center gap-2">
+                  {row.status === 'Pending' && (
+                    <RiProgress1Line className="text-amber-600" size={24} />
+                  )}
+                  {row.status === 'Completed' && (
+                    <IoCheckmarkDoneCircle
+                      className="text-emerald-600"
+                      size={24}
+                    />
+                  )}
+                  {row.status === 'Rejected' && (
+                    <TiWarning className="text-red-600" size={24} />
+                  )}
+                </td>
+                <td className="py-2 px-2">{row.source}</td>
+                <td className="py-2 px-2 text-black dark:text-gray-400 text-base font-medium">
+                  ${row.amount}.00
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
