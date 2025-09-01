996a1f83a47379394d68fdf4fadd6958503c09b8
sathisk15 <rsathisk15@gmail.com>
2025-09-01 14:11:09 +0530

    Account Info added to dashboard

--- src/components/ui/dashboard/Accounts.jsx
+++ src/components/ui/dashboard/Accounts.jsx
@@ -2,31 +2,14 @@ import React from 'react';
 import { FaBtc } from 'react-icons/fa';
 import { RiVisaLine } from 'react-icons/ri';
 import Title from '../shared/Title';
+import {
+  formatCurrency,
+  getIcon,
+  maskAccountNumber,
+} from '../../../utils/utils';
 
-const data = [
-  {
-    name: 'Crypto',
-    account: 'test@gmail.com',
-    amount: '85,323.97',
-    icon: (
-      <div className="w-12 h-12 bg-amber-600 text-white">
-        <FaBtc size={26} />
-      </div>
-    ),
-  },
-  {
-    name: 'Visa Debit Card',
-    account: '2342********9834',
-    amount: '65,323.97',
-    icon: (
-      <div className="w-12 h-12 bg-blue-600 text-white">
-        <RiVisaLine size={26} />
-      </div>
-    ),
-  },
-];
-
-const Accounts = () => {
+const Accounts = ({ accounts }) => {
+  console.log(accounts);
   return (
     <div className="mt-20 md:mt-0 py-5 md:py-20 md:w-1/3">
       <Title title={'Accounts'} />
@@ -34,25 +17,35 @@ const Accounts = () => {
         View all your accounts
       </span>
       <div className="w-full">
-        {data.map((item, idx) => (
-          <div key={idx} className="flex items-center justify-between mt-6">
-            <div className="flex items-center gap-4">{item.icon}</div>
-            <div>
-              <p className="text-black dark:text-gray-400 text-lg">
-                {item.name}
-              </p>
-              <span className="text-gray-600">{item.account}</span>
+        {accounts?.map((account, idx) => {
+          const { account_name, account_number, account_balance } = account;
+          const [Icon, color] = getIcon(account_name?.toLowerCase());
+          return (
+            <div key={idx} className="flex items-center justify-between mt-6">
+              <div className="flex items-center gap-4">
+                <div className={`w-12 h-12 bg-${color}-600 text-white`}>
+                  <Icon size={26} />
+                </div>
+              </div>
+              <div>
+                <p className="text-black dark:text-gray-400 text-lg">
+                  {account_name}
+                </p>
+                <span className="text-gray-600">
+                  {maskAccountNumber(account_number)}
+                </span>
+              </div>
+              <div>
+                <p className="text-xl text-black dark:text-gray-400 font-medium">
+                  {formatCurrency(account_balance)}
+                </p>
+                <span className="text-xl text-gray-600 dark:text-violet-700">
+                  Account Balance
+                </span>
+              </div>
             </div>
-            <div>
-              <p className="text-xl text-black dark:text-gray-400 font-medium">
-                ${item.amount}.00
-              </p>
-              <span className="text-xl text-gray-600 dark:text-violet-700">
-                Account Balance
-              </span>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
