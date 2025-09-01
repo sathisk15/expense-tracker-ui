937bb2fa128b360bba01fe009c676a83bacd3675
sathisk15 <rsathisk15@gmail.com>
2025-09-01 13:45:05 +0530

    Dashboard API Integrated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,8 +4,34 @@ import Chart from '../components/ui/dashboard/Chart';
 import Transactions from '../components/ui/dashboard/Transactions';
 import DoughnutChart from '../components/ui/dashboard/DouhgnutChart';
 import Accounts from '../components/ui/dashboard/Accounts';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDashboardInfo } from '../store/features/transactionSlice';
+import Loading from '../components/ui/shared/Loading';
+import { formatCurrency } from '../utils/utils';
 
 const Dashboard = () => {
+  const { isLoading, data } = useSelector(
+    ({ transaction }) => transaction.dashboardInfo
+  );
+
+  const {
+    availableBalance,
+    totalIncome,
+    totalExpense,
+    chartData,
+    lastTransaction,
+    lastAccount,
+  } = data;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDashboardInfo());
+  }, [dispatch]);
+
+  if (isLoading) return <Loading />;
+
   return (
     <div className="">
       <div className="px-0 md:px-5 2xl:px-20">
@@ -13,15 +39,19 @@ const Dashboard = () => {
           title={'Dashboard'}
           subTitle={'Monitor your financial activities'}
         />
-        <Stats />
+        <Stats
+          balance={formatCurrency(availableBalance)}
+          income={formatCurrency(totalIncome)}
+          expense={formatCurrency(totalExpense)}
+        />
         <div className="w-full flex flex-col-reverse md:flex-row items-center gap-10">
-          <Chart />
-          <DoughnutChart />
+          <Chart data={chartData} />
+          <DoughnutChart income={totalIncome} expense={totalExpense} />
         </div>
 
         <div className="flex flex-col-reverse md:flex-row gap-0 md:gap-10 2xl:gap-20">
-          <Transactions />
-          <Accounts />
+          <Transactions transaction={lastTransaction} />
+          <Accounts accounts={lastAccount} />
         </div>
       </div>
     </div>
