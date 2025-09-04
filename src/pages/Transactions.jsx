d460f20c7b87e2212e8e26218d06364e18321078
sathisk15 <rsathisk15@gmail.com>
2025-09-04 16:08:56 +0530

    Transactions main componenet updated

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Title from '../components/ui/shared/Title';
-import { IoSearchOutline } from 'react-icons/io5';
+import { IoCheckmarkDoneCircle, IoSearchOutline } from 'react-icons/io5';
 import { MdAdd } from 'react-icons/md';
 import { CiExport } from 'react-icons/ci';
 import DateRange from '../components/ui/shared/DateRange';
+import { RiProgress3Line } from 'react-icons/ri';
+import { TiWarning } from 'react-icons/ti';
+import { formatCurrency } from '../utils/utils';
 
 const Transactions = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -72,6 +75,86 @@ const Transactions = () => {
               </span>
             </button>
           </div>
+        </div>
+        <div className="overflow-x-auto mt-5">
+          {data.length === 0 ? (
+            <div className="w-full flex items-center justify-center py-10 text-gray-600 dark:text-gray-700 text-lg">
+              <span>No Transaction History</span>
+            </div>
+          ) : (
+            <>
+              <table className="w-full">
+                <thead className="w-full border-b border-gray-300 dark:border-gray-700">
+                  <tr className="w-full text-black dark:text-gray-400 text-left">
+                    <th className="py-2">Date</th>
+                    <th className="p-2">Description</th>
+                    <th className="p-2">Status</th>
+                    <th className="p-2">Source</th>
+                    <th className="p-2">Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data?.map((transaction, idx) => (
+                    <tr
+                      key={idx}
+                      className="w-full border-gray-200 dark:border-gray-700 text-gray-600 dark:text-gray-500 hover:bf-gray-300/10 text-sm md:text-base cursor-pointer"
+                    >
+                      <td className="py-4">
+                        <p className="w-24 md:w-auto">
+                          {new Date(transaction.createdat).toDateString()}
+                        </p>
+                      </td>
+                      <td className="py-4 px-2">
+                        <div className="flex flex-col w-56 md:w-auto">
+                          <p className="text-base 2xl:text-lg text-black dark:text-gray-400 line-clamp-2">
+                            {transaction.description}
+                          </p>
+                        </div>
+                      </td>
+                      <td className="py-4 px-2">
+                        <div className="flex items-center gap-2">
+                          {transaction.status === 'pending' && (
+                            <RiProgress3Line
+                              className="text-amber-600"
+                              size={24}
+                            />
+                          )}
+                          {transaction.status === 'completed' && (
+                            <IoCheckmarkDoneCircle
+                              className="text-emerald-600"
+                              size={24}
+                            />
+                          )}
+                          {transaction.status === 'rejected' && (
+                            <TiWarning className="text-red-600" size={24} />
+                          )}
+                          <span>{transaction.status}</span>
+                        </div>
+                      </td>
+                      <td className="py-4 px-2">{transaction?.source}</td>
+                      <td className="py-4 text-black dark:text-gray-400 text-base font-medium">
+                        <span
+                          className={`${
+                            transaction?.type === 'income'
+                              ? 'text-emerald-600'
+                              : 'text-red-600'
+                          } text-lg font-bold mgl-1`}
+                        >
+                          {transaction?.type === 'income' ? '+' : '-'}
+                        </span>
+                        {formatCurrency(transaction.amount)}
+                      </td>
+                      <td className="py-2 px-2">
+                        <button className="outline-none text-violet-600 hover:underline">
+                          View
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
         </div>
       </div>
     </>
